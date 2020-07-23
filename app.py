1b420cffb5e40618db7dfdf60ed210480963b3b8
premson.kp@gmail.com <Karkares@1992>
2020-07-24 00:52:15 +0400

    image recognition commit

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import requests
-from flask import Flask, render_template, Response, jsonify, json
+from flask import Flask, render_template, Response, jsonify, json, request
 from camera import VideoCamera
 import cv2
+from cloudant.client import Cloudant
+from cloudant.error import CloudantException
+from cloudant.result import Result, ResultByKey
 
 app = Flask(__name__)
 
@@ -34,16 +37,76 @@ def onClick():
     output = requests.post(url = URL, data = json.dumps(myobj))
 
     testResponse = output.json()
-    outputObj = {'OutputPersonName': testResponse["PersonName"]}
+
+    getDetailsInpObj = {'personname': testResponse["PersonName"]}
+    # getDetailsInpObj = {'personname': 'Niranjan'}
+    getDetailsInpObj = jsonify(getDetailsInpObj)
+    # print(getDetailsInpObj)
+    getDetailsOutObj = _getDetailsFromCloudant(getDetailsInpObj)
+    # print(str(getDetailsOutObj))
+    tempstr = str(getDetailsOutObj)
+    tempstr = tempstr[1:-1]
+    tempstr = tempstr.replace("':", "\":")
+    tempstr = tempstr.replace("', '", "', \"")
+    tempstr = tempstr.replace("{'", "{\"")
+    tempstr = tempstr.replace("\":", "\" :")
+    tempstr = tempstr.replace("'", "\"")
+    print(tempstr)
+    tempDict = json.loads(tempstr)
+    # tempstr.replace("'", "\"")
+    # print(json.dumps(tempDict))
+    # print('START----->' + tempstr[1:-1] + '<------END')
+
+
+    # outputObj = {'OutputPersonName': testResponse["PersonName"]}
     # print(outputObj)
-    response = jsonify(outputObj)
+    # response = jsonify(outputObj)
+    response = jsonify(tempDict)
     # response.status_code = 200  # or 400 or whatever
     # return testResponse["PersonName"]
-    print(response)
+    # print(response)
     return response
 
     # return str(str(video_stream.get_frame_string()))
     # return ("nothing")
+
+@app.route('/getDetails', methods=['GET', 'POST','DELETE', 'PATCH'])
+def _getDetailsFromCloudant(person):
+    client = Cloudant("a3197aee-3b78-4540-8417-c536bd4da1b7-bluemix",
+                      "7fb911920babd92e5184cc7139b2760c6ac1c04f44384627de50f3db8d3d441a",
+                      url="https://a3197aee-3b78-4540-8417-c536bd4da1b7-bluemix:7fb911920babd92e5184cc7139b2760c6ac1c04f44384627de50f3db8d3d441a@a3197aee-3b78-4540-8417-c536bd4da1b7-bluemix.cloudant.com")
+
+    client.connect()
+    database_name = "facerecog"
+    my_database = client.create_database(database_name)
+
+    # inputData = request.get_json()
+    # print('input' + str(person.get_json().get('personname')))
+    # print('input' + str(inputData.get('personname')))
+
+    if my_database.exists():
+        # print(f"'{database_name}' database exists.")
+        # my_document = my_database['ee7492a6cb4f98dead06240c6f156207']
+        # my_document = my_database['Niranjan']
+        # selector = {'name': {'$eq': 'Niranjan'}}
+        # selector = {'name': {'$eq': str(inputData.get('personname'))}}
+        selector = {'name': {'$eq': str(person.get_json().get('personname'))}}
+        docs = my_database.get_query_result(selector)
+        dbobj = docs.all()
+        # print(docs.all())
+        # print(dbobj)
+        return dbobj
+        # result_collection = Result(my_database.all_docs, include_docs=True)
+        #
+        # for result in result_collection:
+        #     print(result['doc'])
+        #     # if result['name']=='Niranjan':
+        #     #     outresponse = result
+        #     #     return jsonify(outresponse)
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', debug=True,port="5000")
